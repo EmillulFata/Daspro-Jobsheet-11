4abbbf7969d5062b0190c84c2580a87b3574273e
FataRach <emillulfr@gmail.com>
2025-11-15 17:11:43 +0700

    Modifikasi Percobaan 4 dan Jawaban Pertanyaan

--- Siakad07.java
+++ Siakad07.java
@@ -3,30 +3,38 @@ import java.util.Scanner;
 public class Siakad07 {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
+    System.out.print("Masukkan jumlah siswa: ");
+    int jumlahSiswa = input.nextInt();
 
-    int [][] nilai = new int [4][3];
+    System.out.print("Masukkan jumlah mata kuliah: ");
+    int jumlahMatkul = input.nextInt();
 
-    for (int i = 0; i < nilai.length; i++) {
-      System.out.println("Input nilai mahasiswa ke-" + (i + 1));
+    int[][] nilai = new int[jumlahSiswa][jumlahMatkul];
+
+    for (int i = 0; i < jumlahSiswa; i++) {
+      System.out.println("\nInput nilai mahasiswa ke-" + (i + 1));
       double totalPerSiswa = 0;
-      
-      for (int j = 0; j < nilai[i]. length; j++) {
-        System.out.print("Nilai mata kuliah " + (j+1) + ": ");
+
+      for (int j = 0; j < jumlahMatkul; j++) {
+        System.out.print("Nilai mata kuliah " + (j + 1) + ": ");
         nilai[i][j] = input.nextInt();
         totalPerSiswa += nilai[i][j];
       }
-      System.out.println("Nilai rata-rata: " + totalPerSiswa/3);
+
+      System.out.println("Rata-rata mahasiswa ke-" + (i + 1) + ": " + (totalPerSiswa / jumlahMatkul));
     }
-    
-    System. out. println("\n ==============================");
-    System.out.println("Rata-rata Nilai setiap Mata Kuliah:");
-    
-    for (int j = 0; j < 3; j++) {
+
+    System.out.println("\n================================");
+    System.out.println("Rata-rata Nilai Setiap Mata Kuliah:");
+
+    for (int j = 0; j < jumlahMatkul; j++) {
       double totalPerMatkul = 0;
-      for (int i = 0; i < 4; i++) {
+
+      for (int i = 0; i < jumlahSiswa; i++) {
         totalPerMatkul += nilai[i][j];
       }
-      System.out.println("Mata Kuliah " + (j + 1) + ": " + totalPerMatkul / 4);
-   }
- }
+
+      System.out.println("Mata Kuliah " + (j + 1) + ": " + (totalPerMatkul / jumlahSiswa));
+    }
+  }
 }
---
+++ Siakad07.md
@@ -0,0 +1,5 @@
+Bagaimana jika terdapat perubahan jumlah siswa dan jumlah mata kuliah? Modifikasi 
+kode program SIAKAD untuk mengakomodasi jumlah siswa dan jumlah mata kuliah 
+yang dinamis.
+
+Jawab : Jika terdapat perubahan jumlah siswa dan jumlah mata kuliah, maka program tidak boleh menggunakan ukuran array yang tetap.
